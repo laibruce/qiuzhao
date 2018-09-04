33607376838b315cf23de70a51fb11765bfb36cb
laibruce <632993801@qq.com>
2018-09-04 22:37:01 +0800

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -20,3 +20,5 @@ int binary_search(container &array, int value)
 
 //二分查找的代码及其变种请仔细参考
 //https://www.cnblogs.com/luoxn28/p/5767571.html
+//二分查找注意事项 http://www.sohu.com/a/215243987_465914
+//即注意使用 mid = left + (right - left)/2,而不要使用 mid = (left + right)/2,因为后者可能出现溢出问题。
